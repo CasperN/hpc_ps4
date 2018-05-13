22737dc01b32bfdaa5802d3d22f13d71f6e50bb7
CasperN <casperneo@uchicago.edu>
2018-05-13 14:13:59 -0500

    finished scaling.sh

--- studies/scaling.sh
+++ studies/scaling.sh
@@ -3,22 +3,26 @@
 #SBATCH --partition=sandyb
 #SBATCH --output=out/serial.out
 #SBATCH --error=out/serial.err
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --exclusive
 
 module load mvapich2
 
-
-1,600 2,850 4,1200 8,1704 16,2400 32,3400 64,4800
-
 # weak scaling study
-for i in 1,600 2,850 4,1200 8,1704 16,2400 32,3400 64,4800
+for i in 1,600 2,850 4,1200 8,1700 16,2400 32,3400 64,4800
 do
     IFS=',' read ranks N <<< "${i}"
 
     mpirun -n $ranks ./cg $N parallel
+    echo
 done
 
 # strong scaling study
+for ranks in 1 2 4 8 16 32 64
+do
+    N=1600
+    mpirun -n $ranks ./cg $N parallel
+    echo ''
+done
